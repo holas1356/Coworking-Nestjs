67879afc1a3ceea5a8d9f7fd6142f46bfb8e6399
holas1356 <ruthbustamante075@gmail.com>
2024-06-21 10:20:17 -0500

    se crea DTO para validar los datos del cliente en la creacion y actualizacion

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsOptional, Length, IsPhoneNumber, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 50)
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Length(1, 100)
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 100)
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  @Length(0, 100)
+  full_name?: string;
+
+  @IsPhoneNumber(null)
+  @IsOptional()
+  @Length(0, 15)
+  phone_number?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 20)
+  role: string;
+}
