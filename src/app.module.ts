a127aca5e2af4aaf9ad2c14befbcc5e32b9c44ac
holas1356 <ruthbustamante075@gmail.com>
2024-06-21 10:11:14 -0500

    users scheme is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {  Users } from './users/entities/user.entity';
 
 @Module({
   imports: [
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ length: 50 })
+  username: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ length: 100 })
+  password: string;
+
+  @Column({ length: 100, nullable: true })
+  full_name: string;
+
+  @Column({ length: 15, nullable: true })
+  phone_number: string;
+
+  @Column({ length: 20 })
+  role: string;
+}
