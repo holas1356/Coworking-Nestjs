6767d0d79ba6a608a0373caca9380d970806344f
holas1356 <ruthbustamante075@gmail.com>
2024-06-21 10:56:28 -0500

    DTO is created for the validation of space data

--- src/workspaces/dto/create-workspace.dto.ts
+++ src/workspaces/dto/create-workspace.dto.ts
@@ -1,1 +1,23 @@
-export class CreateWorkspaceDto {}
+import { IsInt, IsString, IsBoolean, IsOptional, Length, Min } from 'class-validator';
+
+export class CreateWorkspaceDto {
+  @IsInt()
+  room_id: number;
+
+  @IsInt()
+  @Min(1)
+  row_number: number;
+
+  @IsInt()
+  @Min(1)
+  column_number: number;
+
+  @IsString()
+  @IsOptional()
+  @Length(0, 50)
+  workspace_type?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  has_power_outlet?: boolean;
+}
