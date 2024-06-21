5c04321624829ed2f9b3c59cdc914bb18fe9b248
holas1356 <ruthbustamante075@gmail.com>
2024-06-21 11:48:50 -0500

    dto is created to validate session data

--- src/sessions/dto/create-session.dto.ts
+++ src/sessions/dto/create-session.dto.ts
@@ -1,1 +1,21 @@
-export class CreateSessionDto {}
+// create-session.dto.ts
+
+import { IsNotEmpty, IsString, IsOptional, IsNumber, IsDateString } from 'class-validator';
+
+export class CreateSessionDto {
+  @IsNotEmpty()
+  @IsString()
+  session_name: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  start_time: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
+  end_time: Date;
+
+  @IsOptional()
+  @IsNumber()
+  max_capacity?: number;
+}
