53564cb009184060a8b5047a017666324544e892
holas1356 <ruthbustamante075@gmail.com>
2024-06-21 10:26:52 -0500

    discount is created to create and update rooms

--- src/rooms/dto/create-room.dto.ts
+++ src/rooms/dto/create-room.dto.ts
@@ -1,1 +1,28 @@
-export class CreateRoomDto {}
+import { IsString, IsInt, IsOptional, IsBoolean, Length, Min } from 'class-validator';
+
+export class CreateRoomDto {
+  @IsString()
+  @Length(1, 100)
+  room_name: string;
+
+  @IsInt()
+  @Min(1)
+  rows: number;
+
+  @IsInt()
+  @Min(1)
+  columns: number;
+
+  @IsString()
+  @IsOptional()
+  @Length(0, 200)
+  location?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  has_projector?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  has_whiteboard?: boolean;
+}
